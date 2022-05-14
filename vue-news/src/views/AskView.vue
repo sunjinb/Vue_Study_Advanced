6651daa9835d2063a68488da75c5565f969e0fa5
Sunjin <tjswls8062@skuniv.ac.kr>
2022-05-15 01:47:18 +0900

    Second Factoring

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,28 +1,34 @@
 <template>
   <div>
-    <div v-for="item in ask">{{ item.title }}</div>
+    <p v-for="item in fetchedAsk">
+      <a v-bind:href="item.url">
+        {{ item.title }}
+      </a>
+      <small>{{ item.time_ago }} by {{ item.user }}</small>
+    </p>
   </div>
 </template>
 
 <script>
-import { fetchAskList } from '../api/index.js';
+import { mapState, mapGetters } from 'vuex';
 
 export default {
-  data() {
-    return {
-      ask: []
-    }
+  computed: {
+    ...mapGetters({
+      fetchedAsk: 'fetchedAsk'
+    }),
+    // 2번쨰 방법
+    // ...mapState({
+    //   ask: state => state.ask
+    // }),
+    // 3번쨰 방법
+    // ask() {
+    //   return this.$store.state.ask;
+    // }
   },
   created() {
-    var vm = this;
-      fetchAskList()
-        .then(function(response) {
-          vm.ask = response.data;
-        })
-        .catch(function(error){
-          console.log(error);
-        });
-  },
+    this.$store.dispatch('FETCH_ASK');
+  }
 }
 </script>
 
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,29 +1,19 @@
 <template>
   <div>
-    <div v-for="user in users">{{ user.title }}</div>
+    <p v-for="item in this.$store.state.news">
+      <a v-bind:href="item.url">
+        {{ item.title }}
+        </a>
+        <small>{{ item.time_ago }} by {{ item.user }}</small>
+      </p>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index.js';
-
 export default {
-  data() {
-    return {
-      users: []
-    }
-  },
   created() {
-    var vm = this;
-      fetchNewsList()
-        .then(function(response) {
-          console.log(response);
-          vm.users = response.data;
-        })
-        .catch(function(error){
-          console.log(error);
-        })
-  },
+    this.$store.dispatch('FETCH_NEWS');
+  }
 }
 </script>
 
