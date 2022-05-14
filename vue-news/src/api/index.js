1676372baaa8852c5086799c55464e8852a5c0ba
Sunjin <tjswls8062@skuniv.ac.kr>
2022-05-15 00:25:27 +0900

    first factoring

---
+++ vue-news/src/api/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+// 1. 기본 HTTP Request & Response와 관련된 기본 설정
+const config = {
+  baseUrl: 'https://api.hnpwa.com/v0/'
+}
+
+// 2. API 함수들을 정리 
+function fetchNewsList() {
+  //return axios.get(config.baseUrl + 'news/1.json');
+  return axios.get(`${config.baseUrl}news/1.json`);  // 두 줄은 동일 `(백틱) 개념
+}
+
+function fetchAskList() {
+  return axios.get(`${config.baseUrl}ask/1.json`);
+}
+
+function fetchJobsList() {
+  return axios.get(`${config.baseUrl}jobs/1.json`);
+}
+
+
+export {
+  fetchNewsList,
+  fetchAskList,
+  fetchJobsList,
+}
---
+++ vue-news/src/components/ToolBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Asks</router-link> |
+    <router-link to="jobs">Jobs</router-link> 
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped> /* 해당 컴포넌트에만 스타일 작성 */
+.header {
+  color: white;
+    background-color: #42b883;
+    display: flex;
+    padding: 8px;
+}
+.header .router-link-exact-active{    /* 활성화 상태 */
+  color: #35495e;
+}
+.header a { /* 활성화 되지 않은 상태 */
+  color: white;
+}
+</style>
